ad7d95c445cee10edd4b46d46deb04c3831738d3
Olha Levko <olha.levko@avenga.com>
2021-05-21 15:25:50 +0300

    added pagination

---
+++ src/main/java/io/swagger/client/exception/InvalidTokenException.java
@@ -0,0 +1,7 @@
+package io.swagger.client.exception;
+
+public class InvalidTokenException extends RuntimeException{
+    public InvalidTokenException(String message) {
+        super(message);
+    }
+}
